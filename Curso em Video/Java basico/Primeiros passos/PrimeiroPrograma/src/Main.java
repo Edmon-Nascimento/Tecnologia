fd0246ddd2b09d821857007fe6d3f0ddb5638cc4
EDMON NASCIMENTO SANTOS <edmoncode7@gmail.com>
2026-02-24 15:57:14 -0300

    .

---
+++ Curso em Video/Java basico/Primeiros passos/PrimeiroPrograma/src/Main.java
@@ -0,0 +1,29 @@
+
+void main() {
+    IO.println("Olá, Mundo");
+}
+
+/*
+    MinhaClasse
+    MinhaInterface
+
+    meuAtributo
+    minhaVariável
+    meuMétodo
+
+    meu_pacote
+
+    MINHA_CONSTANTE
+*/
+
+/*
+
+    package primeiroprograma;
+
+    public class PrimeiroPrograma{
+        public static void main(String[] args){
+            System.out.println("Ola, Mundo!")
+        }
+    }
+
+*/
